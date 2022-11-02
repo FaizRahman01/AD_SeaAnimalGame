6c20b1c58eeefb2c99c4a7dba3eece9f028022d1
Faiz Rahman <mdfaiz.rahman@gmail.com>
2022-11-02 16:31:22 +0800

    delete game page 2

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace AD_SeaAnimalGame.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap buttonbg {
+            get {
+                object obj = ResourceManager.GetObject("buttonbg", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         /// </summary>
         internal static System.IO.UnmanagedMemoryStream correct {
